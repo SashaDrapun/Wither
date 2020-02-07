3bb3a4f378fdee611ed2d395e254af51929b33e7
SashaDrapun <45926089+SashaDrapun@users.noreply.github.com>
2020-02-07 17:52:10 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    loop: true,
+    navigation:{
+        nextEl: '.arrow'
+    },
+
+    breakpoints:{
+        540:{
+            slidesPerView: 2,
+        }
+
+    }
+});
+
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.header');
+
+menuButton.addEventListener('click', function(){
+    menu.classList.toggle('header-active');
+    menuButton.classList.toggle('menu-button-active');
+});
